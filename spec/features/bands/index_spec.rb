2231aae0b59cfb277be019fe0927a2f6edf75037
James Taylor <jrtaylor0614@gmail.com>
2023-02-04 18:44:40 -0700

    Adds user story 7 and index_spec for bands

---
+++ spec/features/bands/index_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe " bands index page" do
+  before do 
+    @concert_1 = Concert.create(name: "RiotFest", capacity: 500, sold_out: false )
+    @band_1 = Band.create(band_name: "Chon", headliner: false, members: 5, concert_id: @concert_1.id )
+    @band_2 = Band.create(band_name: "polyphia", headliner: true, members: 4, concert_id: @concert_1.id )
+  end
+
+  describe " GET /index" do
+    it "shows a list of bands and their attributes" do
+      visit "/bands"
+     expect(page).to have_content(@band_1.band_name)
+     expect(page).to have_content(@band_1.headliner)
+     expect(page).to have_content(@band_1.members)
+     expect(page).to have_content(@band_1.concert_id)
+     expect(page).to have_content(@band_2.band_name)
+     expect(page).to have_content(@band_2.headliner)
+     expect(page).to have_content(@band_2.members)
+     expect(page).to have_content(@band_2.concert_id)
+     
+    end
+  end
+end
--- spec/features/concerts/show_spec.rb
+++ spec/features/concerts/show_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 
 RSpec.describe 'concert show page' do 
-  it 'displays concert name' do
+  it 'displays concert name and attributes' do
     concert = Concert.create!(name: "RiotFest", capacity: 500, sold_out: false)
     concert2 = Concert.create!(name: "RockVill 2023", capacity: 900, sold_out: true)
     
@@ -10,10 +10,12 @@ RSpec.describe 'concert show page' do
     expect(page).to have_content(concert.name)
     expect(page).to have_content(concert.capacity)
     expect(page).to have_content(concert.sold_out)
+    expect(page).to have_content(concert.number_of_bands)
     visit "concerts/#{concert2.id}"
     expect(page).to have_content(concert2.name)
     expect(page).to have_content(concert2.capacity)
     expect(page).to have_content(concert2.sold_out)
+    expect(page).to have_content(concert2.number_of_bands)
 
   end
 end
