24672ba8ec0dc05df65d1a28f5452ebc1971c93c
James Taylor <jrtaylor0614@gmail.com>
2023-02-05 20:40:11 -0700

    Adds user story 11)

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -1,10 +1,26 @@
 class ConcertsController < ApplicationController
+  
+  
   def index
     @concerts = Concert.all.order("created_at DESC")
   end
-
+  
   def show
-   @concert = Concert.find(params[:id])
+    @concert = Concert.find(params[:id])
+  end
+  
+  def new
+  end
+
+  def create
+    # Concert.create(name: params[:name], capacity: params[:capacity], sold_out: params[:sold_out])
+    Concert.create(concert_params)
+    redirect_to '/concerts'
+  end
+
+  private
+  def concert_params
+    params.permit(:name, :capacity, :sold_out)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/concerts', to: 'concerts#index'
+  get '/concerts/new', to: 'concerts#new'
   get '/concerts/:id', to: 'concerts#show'
   get '/bands', to: 'bands#index'
   get '/bands/:id', to: 'bands#show'
   get '/concerts/:concert_id/bands', to: 'concerts/bands#index'
+  post 'concerts', to: 'concerts#create'
+
 end
